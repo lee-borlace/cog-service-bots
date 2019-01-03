caeb3a5b82c5536a37e2bea4b53d56ada01eaf56
Lee Borlace <lee.borlace@gmail.com>
2019-01-03 09:56:34 +0800

    Added initial WatcherBot functionality.

---
+++ WatcherBot/Startup.cs
@@ -0,0 +1,156 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using WatcherBot.Config;
+
+namespace WatcherBot
+{
+    /// <summary>
+    /// The Startup class configures services and the request pipeline.
+    /// </summary>
+    public class Startup
+    {
+        private ILoggerFactory _loggerFactory;
+        private readonly bool _isProduction;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _isProduction = env.IsProduction();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// Gets the configuration that represents a set of key/value application configuration properties.
+        /// </summary>
+        /// <value>
+        /// The <see cref="IConfiguration"/> that represents a set of key/value application configuration properties.
+        /// </value>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> specifies the contract for a collection of service descriptors.</param>
+        /// <seealso cref="IStatePropertyAccessor{T}"/>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/dependency-injection"/>
+        /// <seealso cref="https://docs.microsoft.com/en-us/azure/bot-service/bot-service-manage-channels?view=azure-bot-service-4.0"/>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var secretKey = Configuration.GetSection("botFileSecret")?.Value;
+            var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+            var botConfig = BotConfiguration.Load(botFilePath ?? @".\WatcherBot.bot", secretKey);
+            services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded. ({botConfig})"));
+
+            services.AddOptions();
+            services.Configure<WatcherConfig>(Configuration.GetSection("Watcher"));
+
+            var dataStore = GetBlobDataStore(botConfig);
+
+            var conversationState = GetBlobConversationState(dataStore);
+            var userState = GetBlobUserState(dataStore);
+
+            services.AddSingleton(conversationState);
+            services.AddSingleton(userState);
+
+            services.AddSingleton(new BotStateSet(conversationState, userState));
+
+            services.AddBot<WatcherBotBot>(options =>
+            {
+                // Retrieve current endpoint.
+                var environment = _isProduction ? "production" : "development";
+                var service = botConfig.Services.FirstOrDefault(s => s.Type == "endpoint" && s.Name == environment);
+                if (!(service is EndpointService endpointService))
+                {
+                    throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
+                }
+
+                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+
+                // Creates a logger for the application to use.
+                ILogger logger = _loggerFactory.CreateLogger<WatcherBotBot>();
+
+                // Catches any errors that occur during a conversation turn and logs them.
+                options.OnTurnError = async (context, exception) =>
+                {
+                    logger.LogError($"Exception caught : {exception}");
+                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+
+                options.Middleware.Add(new AutoSaveStateMiddleware(conversationState, userState));
+            });
+
+            services.AddSingleton<WatcherBotAccessors>(sp =>
+            {
+                var accessors = new WatcherBotAccessors(conversationState, userState)
+                {
+                    WatcherBotState = conversationState.CreateProperty<WatcherBotState>(WatcherBotAccessors.WatcherBotStateName),
+                    DialogState = conversationState.CreateProperty<DialogState>(WatcherBotAccessors.DialogStateName),
+                };
+
+                return accessors;
+            });
+        }
+
+        private static ConversationState GetBlobConversationState(IStorage dataStore)
+        {
+            var conversationState = new ConversationState(dataStore);
+            return conversationState;
+        }
+
+        private static UserState GetBlobUserState(IStorage dataStore)
+        {
+            var userState = new UserState(dataStore);
+            return userState;
+        }
+
+        private static IStorage GetBlobDataStore(BotConfiguration botConfig)
+        {
+            const string StorageConfigurationId = "BotStateBlob";
+            var blobConfig = botConfig.FindServiceByNameOrId(StorageConfigurationId);
+            if (!(blobConfig is BlobStorageService blobStorageConfig))
+            {
+                throw new InvalidOperationException($"The .bot file does not contain an blob storage with name '{StorageConfigurationId}'.");
+            }
+            // Default container name.
+            const string DefaultBotContainer = "bot-state-watcherbot";
+            var storageContainer = string.IsNullOrWhiteSpace(blobStorageConfig.Container) ? DefaultBotContainer : blobStorageConfig.Container;
+            IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureBlobStorage(blobStorageConfig.ConnectionString, storageContainer);
+            return dataStore;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
---
+++ WatcherBot/WatcherBotAccessors.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace WatcherBot
+{
+  
+    public class WatcherBotAccessors
+    {
+        public ConversationState ConversationState { get; }
+
+        public UserState UserState { get; }
+
+        public WatcherBotAccessors(ConversationState conversationState, UserState userState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+            UserState = userState ?? throw new ArgumentNullException(nameof(userState));
+        }
+
+        public static string WatcherBotStateName { get; } = $"{nameof(WatcherBotAccessors)}.WatcherBotState";
+
+        public IStatePropertyAccessor<WatcherBotState> WatcherBotState { get; set; }
+
+        public static string DialogStateName { get; } = $"{nameof(WatcherBotAccessors)}.DialogState";
+
+        public IStatePropertyAccessor<DialogState> DialogState { get; set; }
+
+
+
+    }
+}
